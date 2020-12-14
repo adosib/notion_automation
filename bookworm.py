0f807b40b8609c54b93714c2c2778a3e410a507e
adosib <adosib1@gmail.com>
2020-12-13 21:04:46 -0600

    reading from my reading_assignments table

--- bookworm.py
+++ bookworm.py
@@ -3,12 +3,12 @@ import configparser
 
 config = configparser.ConfigParser()
 config.read('notion.cfg')
-token_v2 = config['Notion']['token_v2']
+cfg = config['Notion']
 
 
-client = NotionClient(token_v2=token_v2)
+client = NotionClient(token_v2=cfg['token_v2'])
 
-# Replace this URL with the URL of the page you want to edit
-page = client.get_block("https://www.notion.so/myorg/Test-c0d20a71c0944985ae96e661ccc99821")
+reading = client.get_block(cfg['reading_assignments'])
 
-print("The old title is:", page.title)
+for row_block in reading.collection.get_rows():
+    print(row_block.title, row_block.page_number)
