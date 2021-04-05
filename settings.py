6ff35c9110cfb33e306fb2c5e74ec1a63c3f7f36
adosib <adosib1@gmail.com>
2021-04-05 11:42:03 -0500

    commented out logger subclass

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 from notion.client import NotionClient
 import configparser
 from pathlib import Path
-from logger import NotionClientWithLogger # DELETE
+# from logger import NotionClientWithLogger # DELETE
 
 ARTICLES_ID = 'b9de160a-796a-43c7-9e40-11b5e26c7392'
 BOOKS_ID = '0168612e-ea50-4e1c-9750-9b584270b6d0'
@@ -13,3 +13,4 @@ config.read('notion.cfg')
 cfg = config['Notion']
 
 client = NotionClient(token_v2=cfg['token_v2'])
+# client = NotionClientWithLogger(token_v2=cfg['token_v2'])
